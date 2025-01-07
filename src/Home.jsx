e9660af0c295041c1976ee4c8dfa1b84e2b42edc
fakhirula <fakhirula27@gmail.com>
2025-01-07 11:55:55 +0700

    initial commmit

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
   return (
     <>
       <Header />
-      <Login />
-      <Register />
+      {/* <Login />
+      <Register /> */}
       <Footer />
     </>
   )
---
+++ src/layouts/index.jsx
@@ -0,0 +1,7 @@
+export default function Layouts() {
+  return (
+    <div className="container mx-auto">
+
+    </div>
+  )
+}
