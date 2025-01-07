41b295991dc5b0ec2805fc622691623f5d709f34
fakhirula <fakhirula27@gmail.com>
2025-01-07 11:57:08 +0700

    initial commmit

--- src/components/shared/Footer/index.jsx
+++ src/components/shared/Footer/index.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
 
   return (
     <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <div className="container px-5 py-8 mx-auto flex items-center justify-between sm:flex-row flex-col">
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">&copy; { currentYear } —
           <a href="https://github.com/fakhirula" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@fakhirula</a>
         </p>
