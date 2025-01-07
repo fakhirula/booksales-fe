97d64cc72c1f850423d0111e93c1f6ef9e380136
fakhirula <fakhirula27@gmail.com>
2025-01-07 21:18:43 +0700

    fix: RWD

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="container mx-auto">
       <nav className="bg-white border-gray-200 px-5 py-2.5 dark:bg-gray-800">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <div className="flex flex-wrap justify-between items-center mx-auto">
               <a href="https://flowbite.com" className="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                   <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BookSales</span>
