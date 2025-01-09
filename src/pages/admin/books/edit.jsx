468a062ead8ead3d2eaf53f5da295b0b13bf07d5
fakhirula <fakhirula27@gmail.com>
2025-01-09 11:02:43 +0700

    fix: refactor routing

--- src/pages/admin/books/edit.jsx
+++ src/pages/admin/books/edit.jsx
@@ -9,7 +9,7 @@ export default function BookEdit() {
           className="border-b border-stroke px-6.5 py-4 dark:border-strokedark"
         >
           <h3 className="font-medium text-black dark:text-white">
-            Add Data
+            Edit Data
           </h3>
         </div>
         <form action="#" className="py-5">
